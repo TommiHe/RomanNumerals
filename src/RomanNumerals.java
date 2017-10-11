5d4a1a377c72828c5cdd03004cd745de507fa8f1
TommiHe <31886486+TommiHe@users.noreply.github.com>
2017-10-11 16:44:09 +0300

    "assignment finished"

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,20 @@
 
 public class RomanNumerals {
+	
+	public final int[] ARABIC = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	public final String[] ROMAN = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	public final String[] FAIL = {"VV","LL","DD"};
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
-	}
+		if(romanNum.contains("VV") || romanNum.contains("LL") ||romanNum.contains("DD")) {
+			System.out.println("You cant repeat V, L or D");
+			return 0;
+		}
+        for (int i= 0; i < ROMAN.length; i++) { 
+			 if(romanNum.startsWith(ROMAN[i])) {
+                return ARABIC[i] + convertToInteger(romanNum.replaceFirst(ROMAN[i], "")); }
+        	}	
+
+return 0;
+}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,122 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertinIto1() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="I";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(1, test);
 	}
-
+	
+	@Test
+	public void convertinIIIto3() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="III";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(3, test);
+	}
+	
+	@Test
+	public void convertinVto5() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="V";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(5, test);
+	}
+	
+	@Test
+	public void convertinMDto1500() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="MD";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(1500, test);
+	}
+	
+	@Test
+	public void convertinIVto4() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="IV";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(4, test);
+	}
+	
+	@Test
+	public void convertinMCMLXXXIVto1984() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="MCMLXXXIV";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(1984, test);
+	}
+	
+	@Test
+	public void convertinIVVtofail() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="IVV";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(0, test);
+	}
+	
+	@Test
+	public void cantSubtractVLD() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="VX";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(0, test);
+	}
+	
+	@Test
+	public void convertMMMCCXLIIIto3243() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="MMMCCXLIII";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(3243, test);
+	}
+	
+	@Test
+	public void cantRepeatIXCMmoreThan3Times() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="IIII";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(0, test);
+	}
+	
+	@Test
+	public void canSubstractOnlyOnes() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="DIIX";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(0, test);
+	}
+	
+	@Test
+	public void cantRepeatVLD() {
+		RomanNumerals roman = new RomanNumerals();
+		String romanNum ="VV";
+		
+		int test = roman.convertToInteger(romanNum);
+		//System.out.println(test);
+		assertEquals(0, test);
+	}
 }
